64b4035a27b9bb552555183296fb0e747b442335
coding-workshop <40839357+coding-workshop@users.noreply.github.com>
2018-09-11 18:29:02 -0400

    Initial commit

---
+++ src/app/AppComponent.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class AppComponent extends React.Component {
+  render() {
+    return <div className="app-component">App!</div>;
+  }
+}
+
+export default AppComponent;
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+import { render } from "react-dom";
+import AppComponent from "./app/AppComponent";
+
+render(<AppComponent />, document.getElementById("root"));
